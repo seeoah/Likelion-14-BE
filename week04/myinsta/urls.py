e8ca4162c0112e99d7366a1f2c2e02fe32cae7e5
seoah <24seoah@sookmyung.ac.kr>
2026-06-23 10:13:08 +0900

    feat : @api_view + quert param 계산기 구현

--- week04/myinsta/urls.py
+++ week04/myinsta/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views_cal import calculator_query
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('calculator/query/', calculator_query, name='calculator_query'),
 ]
 
 if settings.DEBUG:
---
+++ week04/posts/views_cal.py
@@ -0,0 +1,29 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response    
+
+@api_view(['GET'])
+def calculator_query(request):
+    num1 = request.GET.get('num1', 0)
+    num2 = request.GET.get('num2', 0)
+    op = request.GET.get('op')
+
+    num1 = int(num1)
+    num2 = int(num2)
+
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        result = num1 / num2
+    else:
+        result = 0
+
+    data = {
+        'op' : op,
+        'result' : result
+    }
+
+    return Response(data)
