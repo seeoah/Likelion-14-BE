2c61ad7f547090aad0c327341a48ec5d0d848cb2
seoah <24seoah@sookmyung.ac.kr>
2026-06-23 09:52:14 +0900

    feat : PostModelViewSet 구현

--- week04/posts/serializers.py
+++ week04/posts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Post
 
-class PostSerializer(ModelSerializer):
+class PostModelSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
--- week04/posts/views.py
+++ week04/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializers import PostModelSerializer
 
 # Create your views here.
+
+class PostModelViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostModelSerializer
